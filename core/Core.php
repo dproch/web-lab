ef772e2271fb4c96886b6c1f8c55b39d222f79f8
munspel <munspel@Matmod-5>
2016-02-23 09:31:51 +0200

    Пример 1. Простейший пакет с демонстрацией зависимостей и автоматической загрузкой классов по стандарту psr-4.

---
+++ core/Core.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WebLab;
+
+/**
+ * Core - test core app class
+ * @author Andrey Lisnyak <munspel@ukr.net>
+ */
+class Core {
+
+    public function __construct() {
+        echo "Тестовый класс создан!";
+    }
+}
